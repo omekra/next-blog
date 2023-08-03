6f3834935b975f9ae883d62c3c8b070aef678688
Fued Kraide <omekra@hotmail.com>
2023-08-03 11:44:24 +1000

    footer and hom page styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./footer.module.css";
 
 const Footer = () => {
@@ -5,7 +6,36 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©{year} Lamania. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="facebook"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="twitter"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="you-tube"
+        />
+      </div>
     </div>
   );
 };
