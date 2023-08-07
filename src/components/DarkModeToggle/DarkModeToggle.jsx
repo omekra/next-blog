43d6e39836ddcc261e05d7a25d996228f105d839
Fued Kraide <omekra@hotmail.com>
2023-08-07 15:42:16 +1000

    dark mode and context

---
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -0,0 +1,19 @@
+import { ThemeContext } from "@/context/ThemeContext";
+import styles from "./darkModeToggle.module.css";
+import { useContext } from "react";
+
+const DarkModeToggle = () => {
+  const { toggle, mode } = useContext(ThemeContext);
+
+  return (
+    <div className={styles.container} onClick={toggle}>
+      <div className={styles.icon}>🌙</div>
+      <div className={styles.icon}>🔆</div>
+      <div
+        className={styles.ball}
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      />
+    </div>
+  );
+};
+export default DarkModeToggle;
