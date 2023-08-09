d764d023ecb3ce6a8afb3319ec169b95ac148c3a
Fued Kraide <omekra@hotmail.com>
2023-08-09 15:11:03 +1000

    sign in with email and password

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -3,8 +3,32 @@ import { signIn } from "next-auth/react";
 import styles from "./page.module.css";
 
 const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn("credentials", { email, password });
+  };
+
   return (
     <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className={styles.input}
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className={styles.input}
+          required
+        />
+        <button className={styles.button}>Login</button>
+      </form>
       <button onClick={() => signIn("google")}>Login with Google</button>
     </div>
   );
